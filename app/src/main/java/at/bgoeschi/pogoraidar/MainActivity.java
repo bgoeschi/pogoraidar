8bf781a1a481a7ef404d8959947c0fd0c2f18237
Thomas Sigmund <thomas.sigmund88@gmail.com>
2017-07-08 16:45:05 +0200

    Add handling if network location is disabled

--- app/src/main/java/at/bgoeschi/pogoraidar/MainActivity.java
+++ app/src/main/java/at/bgoeschi/pogoraidar/MainActivity.java
@@ -7,12 +7,16 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 import com.twitter.sdk.android.core.TwitterCore;
 
@@ -22,6 +26,7 @@ import at.bgoeschi.pogoraidar.tweetlist.TweetListFragment;
 public class MainActivity extends AppCompatActivity implements LoginFragment.OnTwitterResultListener {
 
 	private static final int PERMISSIONS_LOCATION_REQUEST = 22;
+	private static final int LOCATION_SETTINGS_REQUEST = 21;
 	private final String TAG_CURRENT_FRAGMENT = "currentFragment";
 
 	@Override
@@ -40,13 +45,20 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.OnT
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		getSupportFragmentManager().findFragmentByTag(TAG_CURRENT_FRAGMENT).onActivityResult(requestCode, resultCode, data);
+
+		if (requestCode == LOCATION_SETTINGS_REQUEST) {
+			getLocation();
+		} else {
+			Fragment fragment = getSupportFragmentManager().findFragmentByTag(TAG_CURRENT_FRAGMENT);
+			if (fragment != null) {
+				fragment.onActivityResult(requestCode, resultCode, data);
+			}
+		}
 	}
 
 	private void getLocation() {
 		// Get the location manager
-		final LocationManager locationManager = (LocationManager)
-				getSystemService(LOCATION_SERVICE);
+		final LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
 		if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 			// Here, thisActivity is the current activity
@@ -75,16 +87,31 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.OnT
 				@Override
 				public void onStatusChanged(String s, int i, Bundle bundle) {
 					Log.d("LocationManager", "status changed");
+					locationManager.removeUpdates(this);
 				}
 
 				@Override
 				public void onProviderEnabled(String s) {
 					Log.d("LocationManager", "provider enabled");
+					locationManager.removeUpdates(this);
 				}
 
 				@Override
 				public void onProviderDisabled(String s) {
 					Log.d("LocationManager", "provider disabled");
+					locationManager.removeUpdates(this);
+					if ("network".equals(s)) {
+						final Snackbar snackbar = Snackbar.make(findViewById(R.id.content), R.string.network_provider_disabled, Snackbar
+								.LENGTH_INDEFINITE);
+						snackbar.setAction(R.string.settings, new View.OnClickListener() {
+							@Override
+							public void onClick(View view) {
+								startActivityForResult(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS), LOCATION_SETTINGS_REQUEST);
+								snackbar.dismiss();
+							}
+						});
+						snackbar.show();
+					}
 				}
 			}, null);
 		}
